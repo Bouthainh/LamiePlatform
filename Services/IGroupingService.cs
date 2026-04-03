e4162e46c64ec48c39b28b8976c419fb764ecd7c
deemaoaloufi <deemaoaloufi@gmail.com>
2026-04-04 01:24:52 +0300

    Add ChildGroup model and IGroupingService interface

---
+++ Services/IGroupingService.cs
@@ -0,0 +1,10 @@
+﻿using BadeePlatform.Models;
+
+namespace BadeePlatform.Services;
+
+public interface IGroupingService
+{
+    Task<List<ChildGroup>> CreateGroupsForClassAsync(Guid classId);
+    Task<List<ChildGroup>> GetGroupsByClassAsync(Guid classId);
+    Task<bool> ClassAlreadyGroupedAsync(Guid classId);
+}
