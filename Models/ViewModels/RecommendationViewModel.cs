9ba7e32b7a610909aae7e16fc97519dd739e9759
Bouthainh <Bouthainhabdullah5@gmail.com>
2026-04-04 02:07:13 +0300

    Add view models and DTOs for recommendations feature: - GptRecommendationResult DTO for GPT-based activity results - RecommendationDetailViewModel for detailed recommendation info - RecommendationListItem for list displays - RecommendationViewModel for grouping recommendations by child

---
+++ Models/ViewModels/RecommendationViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LamiePlatform.Models.ViewModels
+{
+    public class RecommendationViewModel
+    {
+        public string ChildId { get; set; }
+        public List<RecommendationListItem> Recommendations { get; set; } = new();
+    }
+}
