1ea8af0f14a290e13bd1ed29bb16bf9e39c1d703
deemaoaloufi <deemaoaloufi@gmail.com>
2026-04-04 01:26:32 +0300

    Register GroupingService and add grouping link to educator pages

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using BadeePlatform.Data;
+using BadeePlatform.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -26,6 +27,7 @@ builder.Services.AddScoped<BadeePlatform.Services.IChatService, BadeePlatform.Se
 builder.Services.AddScoped<IPasswordHasher<BadeePlatform.Models.Parent>, PasswordHasher<BadeePlatform.Models.Parent>>();
 builder.Services.AddScoped<IPasswordHasher<BadeePlatform.Models.Educator>, PasswordHasher<BadeePlatform.Models.Educator>>();
 builder.Services.AddScoped<BadeePlatform.Services.IRequestService, BadeePlatform.Services.RequestService>();
+builder.Services.AddScoped<BadeePlatform.Services.IGroupingService, BadeePlatform.Services.GroupingService>();
 
 
 // Add services to the container.
