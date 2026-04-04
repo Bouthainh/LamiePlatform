4a3030b58edba0d3724e6108aeb2bc2d47e5c547
Bouthainh <Bouthainhabdullah5@gmail.com>
2026-04-04 07:14:18 +0300

    Adding the Chat feature to Parent Dashboard

--- Models/ViewModels/ChildDashboardViewModel.cs
+++ Models/ViewModels/ChildDashboardViewModel.cs
@@ -8,6 +8,8 @@
         public string IconImgPath { get; set; }
         public string EducatorName { get; set; }
         public string EducatorEmail { get; set; }
+        public string EducatorId { get; set; }
+        public string ParentId { get; set; }
         public List<ChartItemViewModel> Top3Intelligences { get; set; }
         public List<ChartItemViewModel> AllIntelligences { get; set; }
         public List<string> Activities { get; set; }
