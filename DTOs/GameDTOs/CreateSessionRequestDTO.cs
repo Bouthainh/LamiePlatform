0e164eb697a9b8ea83b463c3c5ba5b6818767cf5
JoudJ <joudjalal5@gmail.com>
2026-04-14 03:21:38 +0300

    Adding Game DTOs

---
+++ DTOs/GameDTOs/CreateSessionRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace LamiePlatform.DTOs.GameDTOs
+{
+    public class CreateSessionRequest
+    {
+        public string ChildId { get; set; }
+        public Guid LevelId { get; set; }
+        public float TotalTime { get; set; }
+    }
+}
---
+++ DTOs/GameDTOs/SaveGameResultRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace LamiePlatform.DTOs.GameDTOs
+{
+    public class SaveGameResultRequest
+    {
+        public string ChildId { get; set; }
+        public string LevelId { get; set; }
+        public float TotalTime { get; set; }
+        public AspectResultDto AspectResult { get; set; }
+    }
+}
