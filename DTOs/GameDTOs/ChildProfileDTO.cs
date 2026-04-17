ab8118af4846c542ce6ec724200ef58a21c9cb3c
JoudJ <joudjalal5@gmail.com>
2026-04-17 13:24:28 +0300

    Add ChildProfileDTO

---
+++ DTOs/GameDTOs/ChildProfileDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace LamiePlatform.DTOs.GameDTOs
+{
+    public class ChildProfileDto
+    {
+        public string FullName { get; set; }
+        public int? Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
