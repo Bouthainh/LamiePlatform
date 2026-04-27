67527b6f0217be533785025f20cd63ec7a4afc08
Bouthainh <bouthainhabdullah5@gmail.com>
2026-04-27 18:27:35 +0300

    Craeting Report Service, and an interface

---
+++ Services/IReportService.cs
@@ -0,0 +1,9 @@
+﻿using LamiePlatform.Models.ViewModels.DashboardViewModels;
+namespace LamiePlatform.Services
+{
+    public interface IReportService
+    {
+        Task<string> GenerateReportAsync(string childId, string viewerRole, string generatedBy);
+        List<ReportFileViewModel> GetChildReports(string childId, string viewerRole);
+    }
+}
